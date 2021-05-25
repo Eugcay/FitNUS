6a58fe0d82cb42985b1a6a94acaa16eb799d1ee7
Eugcay <eugene.chiay@gmail.com>
2021-05-25 18:01:21 +0800

    fitBud and Other assets

---
+++ pages/fitBud.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import HeaderTitle from "./pages/fitBudComponents/header";
+import HistoryBar from "./pages/fitBudComponents/historyBar";
+import MenuButton from "./pages/fitBudComponents/menuButton";
+import FitBudSuggests from "./pages/fitBudComponents/fitBudSuggests";
+import WorkoutSearch from "./pages/fitBudComponents/workoutSearch";
+import WorkoutSearchButton from "./pages/fitBudComponents/workoutSearchButton";
+
+export default function fitBud() {
+    return (
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+});
