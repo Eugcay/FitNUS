6e62e2296b1818d413c67ce2692258d416ecb272
rushilramesh <rushilramesh@gmail.com>
2021-05-27 13:29:28 +0800

    config added for firebase

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBgMd5BQmPXXbwkg5EWNjWSlsrLURAby44",
-    authDomain: "fitnus-b7f2b.firebaseapp.com",
-    projectId: "fitnus-b7f2b",
-    storageBucket: "fitnus-b7f2b.appspot.com",
-    messagingSenderId: "829352565865",
-    appId: "1:829352565865:web:25dc42f57425efd75016f8"
+  apiKey: "AIzaSyBgMd5BQmPXXbwkg5EWNjWSlsrLURAby44",
+  authDomain: "fitnus-b7f2b.firebaseapp.com",
+  projectId: "fitnus-b7f2b",
+  storageBucket: "fitnus-b7f2b.appspot.com",
+  messagingSenderId: "829352565865",
+  appId: "1:829352565865:web:25dc42f57425efd75016f8",
 };
