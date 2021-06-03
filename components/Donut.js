feac9409d866584bebbd4f5368f7448183e1bbfa
Eugcay <eugene.chiay@gmail.com>
2021-06-03 23:43:57 +0800

    DONUT STUFF

--- components/Donut.js
+++ components/Donut.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { View, Easing, Animated, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Easing,
+  Animated,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
 import Svg, { G, Circle } from "react-native-svg";
 
-const ProgressCircle = Animated.createAnimatedComponent(Circle)
+const ProgressCircle = Animated.createAnimatedComponent(Circle);
 
 const Donut = (props) => {
-  const percentage = props.percentage
-  const color = props.color ? props.color : 'tomato'
-  
-   
-  const RADIUS = 100;
-  const STROKEWIDTH = 15;
-  const COLOR = "tomato";
-  const DURATION = 500;
+  const percentage = props.percentage;
+  const color = props.color ? props.color : "#0B2A59";
+
+  const RADIUS = 200;
+  const STROKEWIDTH = 30;
+  const COLOR = "#0B2A59";
+
+  const DURATION = "50"; //seconds to mins
+  const WORKOUT = "Boxing"; //Change to workout object from database
+  //const WORKOUTNAME = "Name";
+  const UNITS = "minuites";
+  const CALORIES = "500Kcal";
+  const DISTANCE = "5KM";
 
   const half = RADIUS + STROKEWIDTH;
   const circumference = 2 * Math.PI * RADIUS;
@@ -20,11 +33,12 @@ const Donut = (props) => {
   return (
     <View style={styles.container}>
       <Svg
-        width={RADIUS * 2}
-        height={RADIUS * 2}
+        width={Dimensions.get("window").width}
+        height={Dimensions.get("window").width}
         viewBox={`0 0 ${half * 2} ${half * 2}`}
+        style={(position = "absolute")}
       >
-        <G rotation='-90' origin={`${half}, ${half}`}>
+        <G rotation="-90" origin={`${half}, ${half}`}>
           <Circle
             cx="50%"
             cy="50%"
@@ -42,11 +56,19 @@ const Donut = (props) => {
             r={RADIUS}
             strokeOpacity={0.8}
             strokeDasharray={circumference}
-            strokeDashoffset={0.4 * circumference}
-            strokeLinecap='round'
+            strokeDashoffset={0.1 * circumference}
+            strokeLinecap="round"
           />
         </G>
       </Svg>
+      <View style={styles.innercircle}>
+        <Text style={styles.bigwords}>{DURATION}</Text>
+        <Text style={styles.smallwords}>{UNITS}</Text>
+        <Text style={styles.smallwords}>of</Text>
+        <TouchableOpacity style={styles.specialbox}>
+          <Text style={styles.specialwords}>{WORKOUT}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -57,6 +79,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    marginVertical: 10,
+    position: "relative",
   },
+  innercircle: {
+    position: "absolute",
+    alignItems: "center",
+    left: "0.5"
+  },
+  bigwords: {
+    textAlign: "center"
+  },
+  smallwords: {
+    textAlign: "center"
+  },
+  specialwords:{
+    textAlign: "center"
+  },
+  specialbox: {
+    alignSelf: "center"
+  }
 });
